bd95f43f4a8e593f0c3c4f03908fa1132338c4e5
Axel Santoso <axcelsantoso0@gmail.com>
2024-02-12 14:11:08 +0700

    sidebar and top bar done.

--- app/src/main/java/com/example/materialfilejetpackcompose/HomePage.kt
+++ app/src/main/java/com/example/materialfilejetpackcompose/HomePage.kt
@@ -1,30 +1,16 @@
 package com.example.materialfilejetpackcompose
 
-import android.content.Context
 import android.widget.Toast
-import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.animation.SizeTransform
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.keyframes
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.expandHorizontally
 import androidx.compose.animation.expandIn
-import androidx.compose.animation.expandVertically
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
-import androidx.compose.animation.shrinkHorizontally
 import androidx.compose.animation.shrinkOut
-import androidx.compose.animation.shrinkVertically
-import androidx.compose.animation.with
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.selection.selectable
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -33,36 +19,84 @@ import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Switch
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.scale
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Yellow
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.semantics.Role.Companion.Switch
-import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun MyApp() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "home") {
+        composable("home") { HomePage(navController) }
+        composable("settings") { SettingsPage(navController) }
+    }
+}
+
+@Composable
+fun SettingsPage(navController: NavController) {
+    Surface {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Settings")
+            Spacer(modifier = Modifier.height(20.dp))
+            Switch(
+                checked = true,
+                onCheckedChange = { /*TODO*/ },
+                modifier = Modifier.padding(16.dp)
+            )
+            Spacer(modifier = Modifier.height(20.dp))
+            Button(onClick = {
+                navController.navigate("home")
+            }) {
+                Text("Go back")
+            }
+        }
+    }
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HomePage() {
+fun HomePage(navController: NavHostController) {
     var isExpanded by remember { mutableStateOf(false) }
-    val widthAnim by animateDpAsState(targetValue = if (isExpanded) 200.dp else 64.dp)
+    val widthAnim by animateDpAsState(targetValue = if (isExpanded) 200.dp else 64.dp, label = "")
 
     Surface {
         Box {
+            TopAppBar(
+                modifier = Modifier
+                    .padding(start = widthAnim)
+                    .fillMaxWidth(),
+                title = { Text("Material Files", color = MaterialTheme.colorScheme.onPrimary) },
+                colors = topAppBarColors(containerColor = MaterialTheme.colorScheme.tertiary)
+            )
             Column(
                 Modifier
                     .fillMaxSize()
-                    .animateContentSize(), // Add this line
+                    .animateContentSize(),
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
@@ -82,31 +116,39 @@ fun HomePage() {
                     .onFocusChanged { focusState ->
                         isExpanded = focusState.isFocused
                     }
-                    .animateContentSize(), // Add this line
-                horizontalAlignment = Alignment.Start
+                    .animateContentSize(),
+                horizontalAlignment = Alignment.Start,
+                verticalArrangement = Arrangement.SpaceBetween
             ) {
                 Icon(
                     Icons.Default.Menu,
                     modifier = Modifier
+                        .padding(top = 10.dp, start = 10.dp)
                         .clickable(
                             onClick = {
                                 isExpanded = !isExpanded
                             }
                         ),
                     contentDescription = null,
-                    tint = Color.White
+                    tint = MaterialTheme.colorScheme.onPrimary
                 )
-                Spacer(modifier = Modifier.height(20.dp))
-                DrawerItem(Icons.Default.Folder, "All Files", isExpanded) {}
-                DrawerItem(Icons.Default.Photo, "Photos", isExpanded) {}
-                DrawerItem(Icons.Default.VideoLibrary, "Videos", isExpanded) {}
-                DrawerItem(Icons.Default.AudioFile, "Audios", isExpanded) {}
-                Spacer(modifier = Modifier.height(80.dp))
+                Column {
+                    Modifier
+                        .padding(top = 10.dp, start = 10.dp)
+                    DrawerItem(Icons.Default.Folder, "All Files", isExpanded) {}
+                    DrawerItem(Icons.Default.Photo, "Photos", isExpanded) {}
+                    DrawerItem(Icons.Default.VideoLibrary, "Videos", isExpanded) {}
+                    DrawerItem(Icons.Default.AudioFile, "Audios", isExpanded) {}
+                }
+                DrawerItem(Icons.Default.Settings, "Settings", isExpanded) {
+                    navController.navigate("settings")
+                }
 
             }
         }
     }
 }
+
 
 @Composable
 fun DrawerItem(icon: ImageVector, title: String, expanded: Boolean, onClick: () -> Unit) {

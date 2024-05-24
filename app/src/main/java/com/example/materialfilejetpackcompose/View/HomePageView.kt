c0fbd5feecb3ec626abc33c36d58cbc995f13e38
Axel Santoso <axcelsantoso0@gmail.com>
2024-05-24 17:32:06 +0700

    change the mutablestateof to mutablefloatstateof

--- app/src/main/java/com/example/materialfilejetpackcompose/View/HomePageView.kt
+++ app/src/main/java/com/example/materialfilejetpackcompose/View/HomePageView.kt
@@ -32,6 +32,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
@@ -281,7 +282,7 @@ class HomePageView(private val navController: NavHostController, private val fil
     }
     @Composable
     fun LinearDeterminateIndicator() {
-        var currentProgress by remember { mutableStateOf(0f) }
+        var currentProgress by remember { mutableFloatStateOf(0f) }
         var loading by remember { mutableStateOf(false) }
         val scope = rememberCoroutineScope() // Create a coroutine scope
 

0e8e2ccc99bc7bc979cdf72b216a85dc78a8fe62
Axel Santoso <axcelsantoso0@gmail.com>
2024-07-16 15:44:02 +0700

    update gradle.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,11 +54,11 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation(platform("androidx.compose:compose-bom:2024.06.00"))
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.tv:tv-foundation:1.0.0-alpha10")
+    implementation("androidx.tv:tv-foundation:1.0.0-alpha11")
     implementation("androidx.compose.foundation:foundation-android:1.6.8")
     implementation("androidx.compose.ui:ui-tooling:1.6.8")
-    implementation("androidx.tv:tv-material:1.0.0-beta01")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.2")
+    implementation("androidx.tv:tv-material:1.0.0-rc01")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.3")
     implementation("androidx.activity:activity-compose:1.9.0")
     implementation("androidx.compose.material3:material3:1.2.1")
     implementation("com.google.firebase:firebase-annotations:16.2.0")
@@ -73,7 +73,7 @@ dependencies {
     //implementation runtime liveData
     implementation("androidx.compose.runtime:runtime-livedata:1.6.8")
     //lifecycle viewmodel compose
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.3")
     //material icons
     implementation("androidx.compose.material:material-icons-core:1.6.8")
     implementation("zandroidx.compose.material:material-icons-extended:1.6.8")

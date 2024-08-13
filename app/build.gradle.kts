dcd08a078e1e0f69e0a7a5e91396908ae2c8c2f4
Axel Santoso <axcelsantoso0@gmail.com>
2024-08-13 10:31:50 +0700

    update gradle.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,7 +57,7 @@ dependencies {
     implementation("androidx.tv:tv-foundation:1.0.0-alpha11")
     implementation("androidx.compose.foundation:foundation-android:1.6.8")
     implementation("androidx.compose.ui:ui-tooling:1.6.8")
-    implementation("androidx.tv:tv-material:1.0.0-rc01")
+    implementation("androidx.tv:tv-material:1.0.0-rc02")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
     implementation("androidx.activity:activity-compose:1.9.1")
     implementation("androidx.compose.material3:material3:1.2.1")

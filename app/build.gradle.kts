a47678f4611a3c2693df0f3cd04d0c1fbb1a3ff8
Axel Santoso <axcelsantoso0@gmail.com>
2024-12-13 14:38:49 +0700

    update gradle and kotlin version.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,16 +1,17 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
 }
 
 android {
     namespace = "com.example.materialfilejetpackcompose"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.materialfilejetpackcompose"
-        minSdk = 30
-        targetSdk = 34
+        minSdk = 32
+        targetSdk = 35
         versionCode = 1
         versionName = "工事中"
         vectorDrawables {
@@ -52,31 +53,31 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation(platform("androidx.compose:compose-bom:2024.11.00"))
+    implementation(platform("androidx.compose:compose-bom:2024.12.01"))
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.tv:tv-foundation:1.0.0-alpha11")
-    implementation("androidx.compose.foundation:foundation-android:1.7.5")
-    implementation("androidx.compose.ui:ui-tooling:1.7.5")
+    implementation("androidx.compose.foundation:foundation-android:1.7.6")
+    implementation("androidx.compose.ui:ui-tooling:1.7.6")
     implementation("androidx.tv:tv-material:1.0.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
     implementation("androidx.activity:activity-compose:1.9.3")
     implementation("androidx.compose.material3:material3:1.3.1")
     implementation("com.google.firebase:firebase-annotations:16.2.0")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2024.11.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.12.01"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.7.5")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.7.6")
     implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.compose.ui:ui:1.7.5")
-    implementation("androidx.compose.material:material:1.7.5")
-    implementation("androidx.navigation:navigation-compose:2.8.4")
+    implementation("androidx.compose.ui:ui:1.7.6")
+    implementation("androidx.compose.material:material:1.7.6")
+    implementation("androidx.navigation:navigation-compose:2.8.5")
     //implementation runtime liveData
-    implementation("androidx.compose.runtime:runtime-livedata:1.7.5")
+    implementation("androidx.compose.runtime:runtime-livedata:1.7.6")
     //lifecycle viewmodel compose
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
     //material icons
-    implementation("androidx.compose.material:material-icons-core:1.7.5")
-    implementation("androidx.compose.material:material-icons-extended:1.7.5")
+    implementation("androidx.compose.material:material-icons-core:1.7.6")
+    implementation("androidx.compose.material:material-icons-extended:1.7.6")
     implementation("io.coil-kt:coil-compose:2.6.0")
     implementation("com.google.accompanist:accompanist-permissions:0.34.0")
     implementation("com.google.accompanist:accompanist-systemuicontroller:0.34.0")
